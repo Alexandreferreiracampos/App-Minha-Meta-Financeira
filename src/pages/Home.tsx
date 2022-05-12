26285d2844fb9220b19efbbf2aa29fbf887aa8b9
Alexandreferreiracampos <50953839+Alexandreferreiracampos@users.noreply.github.com>
2022-05-12 15:38:56 -0300

    config status bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,48 @@
 import react from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, StatusBar} from 'react-native';
 import { Ionicons } from '@expo/vector-icons'; 
 import { FontAwesome } from '@expo/vector-icons'; 
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
 export default function Home(){
 
-    const balance = '6.030,00';
+    const visibleBalance=()=>{
+        
+
+    }
+
+    const balance = '46.030,42';
     const balanceNone = '******'
     
     return(
         <View style={styles.container}>
+            <StatusBar backgroundColor='rgb(18,28,38)' barStyle="light-content" />
             <View style={styles.header}>
-                <View><Text style={{color:'white'}}>Saldo</Text></View>
+                <View><Text style={{color:'white',fontSize:RFPercentage(1.8), top:-10}}>Saldo</Text></View>
                 <View style={styles.containerBalance}>
                     <Text style={styles.balance}>R$ {balance}</Text>
                 </View>
 
-                <View>
-                <Ionicons name="eye-outline" size={25} color="white" />
-                </View>
+                <TouchableOpacity>
+                <Ionicons name="eye-outline" size={RFPercentage(3.5)} color="white" />
+                </TouchableOpacity>
 
                 <View style={styles.ContainerButtomBalance}>
                     <View style={styles.ButtomBalance}>
-                    <FontAwesome name="arrow-circle-up" size={34} color="green" style={{paddingEnd:13}}/>
+                    <FontAwesome name="arrow-circle-up" size={RFPercentage(4.5)} color="green" style={{paddingEnd:13}}/>
                     <View >
-                        <Text style={{color:'white'}}>Entrada</Text>
-                        <Text style={{color:'green',fontSize:20}}>R$ 49.000,80</Text>
+                        <Text style={{color:'white',fontSize:RFPercentage(1.8)}}>Entrada</Text>
+                        <Text style={{color:'green',fontSize:RFPercentage(2.7)}}>R$ 49.000,80</Text>
                     </View>
                     </View>
 
                     <View style={{ width:0.4, height:'70%',backgroundColor:'white', margin:10}}></View>
 
                     <View style={styles.ButtomBalance}>
-                    <FontAwesome name="arrow-circle-down" size={34} color="red" style={{paddingEnd:13}}/>
+                    <FontAwesome name="arrow-circle-down" size={RFPercentage(4.5)} color="red" style={{paddingEnd:13}}/>
                     <View>
-                        <Text style={{color:'white'}}>saída</Text>
-                        <Text style={{color:'red',fontSize:20}}> R$ 1.335,92</Text>
+                        <Text style={{color:'white',fontSize:RFPercentage(1.8)}}>saída</Text>
+                        <Text style={{color:'red',fontSize:RFPercentage(2.7)}}> R$ 1.335,92</Text>
                     </View>
                     </View>
 
@@ -68,14 +75,14 @@ const styles = StyleSheet.create({
         padding:10,
     },
     balance:{
-        fontSize: 36,
+        fontSize: RFPercentage(5),
         color: 'white'
     },
     ContainerButtomBalance:{
         justifyContent:'center',
         alignItems:'center', 
         flexDirection:'row',
-        top:48,
+        top:35,
         
 
     },
